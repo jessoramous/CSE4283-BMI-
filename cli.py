9aba2d2193950dbb20df8da7b0f2ae59b98c94b9
jessoramous <110073863+jessoramous@users.noreply.github.com>
2025-03-18 20:57:42 -0500

    update files

--- cli.py
+++ cli.py
@@ -3,13 +3,13 @@ from bmi import calculate_bmi, bmi_category
 
 def main():
   parser = argparse.ArgumentParser(description="BMI Calculator")
-  parse.add_argument("height_ft", type=int, help="Height in feet")
-  parse.add_argument("height_in", type=int, help="Height in inches")
-  parse.add_argument("weight", type=int, help="Weight in pounds")
+  parser.add_argument("height_ft", type=int, help="Height in feet")
+  parser.add_argument("height_in", type=int, help="Height in inches")
+  parser.add_argument("weight", type=int, help="Weight in pounds")
 
-  args parser.parse_args()
+  args = parser.parse_args()
 
-  bmi = calculate_bmi(args.weight, args.height_ft, args.height_in)
+  bmi = calculate_bmi(args.height_ft, args.height_in, args.weight)
   category = bmi_category(bmi)
 
   print(f"BMI: {bmi}, Category: {category}")
--- test_bmi.py
+++ test_bmi.py
@@ -3,14 +3,16 @@ from bmi import calculate_bmi, bmi_category
 
 class TestBMI(unittest.TestCase):
   def test_calculate_bmi(self):
-    self.assertEqual(calculate_bmi(5, 6, 150), 24.2) # Test normal case
-    self.assertEqual(calculate_bmi(5, 6, 100), 16.2) # Test underweight case
-    self.assertEqual(calculate_bmi(5, 6, 200), 32.3) # Test obese case
+    self.assertEqual(calculate_bmi(5, 6, 150), 24.8) # Test normal case
+    self.assertEqual(calculate_bmi(5, 6, 100), 16.5) # Test underweight case
+    self.assertEqual(calculate_bmi(5, 6, 170), 28.1) # Test overweight case
+    self.assertEqual(calculate_bmi(5, 6, 200), 33.1) # Test obese case
 
   def test_bmi_category(self):
-    self.assertEqual(bmi_category(16.2), "Underweight")
-    self.assertEqual(bmi_category(24.2), "Normal weight")
-    self.assertEqual(bmi_category(32.3), "Obese")
+    self.assertEqual(bmi_category(16.5), "Underweight")
+    self.assertEqual(bmi_category(24.8), "Normal weight")
+    self.assertEqual(bmi_category(28.1), "Overweight")
+    self.assertEqual(bmi_category(33.1), "Obese")
 
 if __name__ == '__main__':
   unittest.main()
