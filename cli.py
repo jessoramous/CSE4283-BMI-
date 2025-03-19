51d2ecafc6be410f06cce2661561f4de502e71c9
jessoramous <110073863+jessoramous@users.noreply.github.com>
2025-03-18 19:58:48 -0500

    add cli.py

---
+++ cli.py
@@ -0,0 +1,18 @@
+import argparse
+from bmi import calculate_bmi, bmi_category
+
+def main():
+  parser = argparse.ArgumentParser(description="BMI Calculator")
+  parse.add_argument("weight", type=int, help="Weight in pounds")
+  parse.add_argument("height_ft", type=int, help="Height in feet")
+  parse.add_argument("height_in", type=int, help="Height in inches")
+
+  args parser.parse_args()
+
+  bmi = calculate_bmi(args.weight, args.height_ft, args.height_in)
+  category = bmi_category(bmi)
+
+  print(f"BMI: {bmi}, Category: {category}")
+
+if __name__ == "__main__":
+  main()
