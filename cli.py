0a01385480c57ba8c87bb7934bd9e1b0c6ec4d7d
jessoramous <110073863+jessoramous@users.noreply.github.com>
2025-03-18 20:05:35 -0500

    update cli.py

--- cli.py
+++ cli.py
@@ -3,9 +3,9 @@ from bmi import calculate_bmi, bmi_category
 
 def main():
   parser = argparse.ArgumentParser(description="BMI Calculator")
-  parse.add_argument("weight", type=int, help="Weight in pounds")
   parse.add_argument("height_ft", type=int, help="Height in feet")
   parse.add_argument("height_in", type=int, help="Height in inches")
+  parse.add_argument("weight", type=int, help="Weight in pounds")
 
   args parser.parse_args()
 
